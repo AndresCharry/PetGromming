20e6777f7c67d6c8c720d4579c01ea7f8c1ad1be
Andres Charry <ingeniero.andres.charry@gmail.com>
2024-03-08 06:05:47 -0500

    structure

---
+++ src/main/java/com/mycompany/petgrooming/PetGrooming.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.petgrooming;
+
+/**
+ *
+ * @author charry
+ */
+public class PetGrooming {
+
+    public static void main(String[] args) {
+        System.out.println("hola World!");
+    }
+}
