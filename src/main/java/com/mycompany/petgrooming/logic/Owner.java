32cab2e44449b269c5b9fc0c1d0028295195fb6b
Andres Charry <ingeniero.andres.charry@gmail.com>
2024-03-08 06:41:49 -0500

    create persistence package and presistence unit

--- src/main/java/com/mycompany/petgrooming/logic/Owner.java
+++ src/main/java/com/mycompany/petgrooming/logic/Owner.java
@@ -1,8 +1,17 @@
 
 package com.mycompany.petgrooming.logic;
 
-public class Owner {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Owner implements Serializable {
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private int id;
     private String name;
     private String phone;
--- src/main/java/com/mycompany/petgrooming/logic/Pet.java
+++ src/main/java/com/mycompany/petgrooming/logic/Pet.java
@@ -1,25 +1,39 @@
 
 package com.mycompany.petgrooming.logic;
 
-public class Pet {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Pet implements Serializable {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private String name;
     private String breed;
     private String color;
     private String Allergic;
     private String specialAttention;
+    @OneToOne
+    private Owner owner;
 
     public Pet() {
     }
 
-    public Pet(int id, String name, String breed, String color, String Allergic, String specialAttention) {
+    public Pet(int id, String name, String breed, String color, String Allergic, 
+            String specialAttention, Owner owner) {
         this.id = id;
         this.name = name;
         this.breed = breed;
         this.color = color;
         this.Allergic = Allergic;
         this.specialAttention = specialAttention;
+        this.owner = owner;
     }
 
     public int getId() {
